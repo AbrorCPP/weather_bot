402e29aeb4910eefa0de1f8b2a4c70e799216b51
AbrorCPP <abrorboltayev4843@gmail.com>
2026-02-16 19:59:53 +0500

    123

--- weather.py
+++ weather.py
@@ -19,13 +19,11 @@ def get_city_name(city:str):
     temp_max = data['main']['temp_max']
     pressure = data['main']['pressure']
     humidity = data['main']['humidity']
-    text = "-"*20
-    text += f"\nBugun {city}da Ob-havo⛅\n"
+    text  = f"\nBugun {city}da Ob-havo⛅\n"
     text += f"\nHarorat {temp} °C 🌡️"
     text += f"\nMaksimal harorat {temp_max} °C"
-    text += f"\nMinimal harorat {temp_min} °C"
+    text += f"\nMinimal harorat {temp_min} °C\n"
     text += f"\nBosim {pressure} Pa"
     text += f"\nNamlik {humidity} 💧 %\n\n"
-    text += "-"*20
     return text
 
